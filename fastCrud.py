234c781119376d53192bea04a2a43f6f541c6e73
Alperen Sümeroğlu <144742696+alperensumeroglu@users.noreply.github.com>
2025-01-29 01:19:31 +0300

    second commit

--- fastCrud.py
+++ fastCrud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -31,4 +31,43 @@ async def get_course_by_id(course_id: int):
     for course in courses_db:
         if course.get("id") == course_id:
             return course
-    
+
+#Query parameter
+@app.get("/courses/?")
+async def get_category_by_query(category: str):
+    courses_to_return = []
+    for course in courses_db:
+        if course.get("category").casefold() == category.casefold():
+            courses_to_return.append(course)
+    return courses_to_return
+
+#Path and Query Parameters
+@app.get("/courses/{course_instructor}/")
+async def get_instructor_category_by_query(course_instructor: str, category: str):
+    courses_to_return = []
+    for course in courses_db:
+        if (course.get('instructor').casefold() == course_instructor.casefold()
+           and course.get('category').casefold() == category.casefold()):
+           courses_to_return.append(course)
+    return courses_to_return
+
+@app.post("/courses/create_course")
+async def create_course(new_course=Body()):
+    courses_db.append(new_course)
+    return new_course
+
+@app.put("/courses/update_course")
+async def update_course(updated_course=Body()):
+    for index in range(len(courses_db)):
+        if courses_db[index].get("id") == updated_course.get("id"):
+            courses_db[index] = updated_course
+
+@app.delete("/courses/delete_course/{course_id}")
+async def delete_course(course_id: int):
+    for index in range(len(courses_db)):
+        if courses_db[index].get("id") == course_id:
+            courses_db.pop(index)
+            break
+    return
+
+
